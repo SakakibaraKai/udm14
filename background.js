1c8148ff0b86d12c34bb2142c6aaec39d70a484e
SakakibaraKai <depwegk@oregonstate.edu>
2024-11-27 16:08:52 -0800

    had umd not udm
    was doing umd not udm updated

--- background.js
+++ background.js
@@ -2,8 +2,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.url && tab.url) {
       let newUrl = tab.url;
       if (newUrl.includes("google.com/search")) {
-        if (!newUrl.includes("&umd=14")) {
-          newUrl += newUrl.includes("?") ? "&umd=14" : "?umd=14";
+        if (!newUrl.includes("&udm=14")) {
+          newUrl += newUrl.includes("?") ? "&udm=14" : "?udm=14";
           chrome.tabs.update(tabId, { url: newUrl });
         }
       }
